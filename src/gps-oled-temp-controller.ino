d3939128dcd861f9278b54f0f319ebd450e80baf
Jakk Sut <jakks@Jakks-iMac.local>
2017-06-14 01:15:21 +0700

    add i2c scanner

--- src/gps-oled-temp-controller.ino
+++ src/gps-oled-temp-controller.ino
@@ -3,7 +3,7 @@
 #include <DHT.h>
 
 #define PIN_RESET 255  //
-#define DC_JUMPER 0  // I2C Addres: 0 - 0x3C, 1 - 0x3D
+#define DC_JUMPER 0  // I2C Addres: 0 - 0x3C, 1 - 0x3D - OLED Display
 
 #define DHTPIN D4
 #define DHTTYPE DHT22
@@ -62,7 +62,11 @@ void initClockVariables()
 
 void setup()
 {
+  Wire.begin();
+
   Serial.begin(9600);
+
+  Serial.println("\nI2C Scanner");
   Serial.println("DHT22 Test!");
 
   dht.begin();
@@ -84,6 +88,8 @@ void loop()
 {
   getTemp();
   
+  scanI2CDevices();
+
   // Check if we need to update seconds, minutes, hours:
   if (lastDraw + CLOCK_SPEED < millis())
   {
@@ -215,4 +221,45 @@ void getTemp() {
   Serial.print(" *C ");
   Serial.print(hif);
   Serial.println(" *F");
+}
+
+void scanI2CDevices() {
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+ 
+  delay(5000);           // wait 5 seconds for next scan
 }
